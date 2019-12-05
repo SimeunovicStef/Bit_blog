db7ad52970bcee7210a46d10e917e9aba9144564
BIT Student <classroom@bgit.rs>
2019-12-05 12:59:50 +0100

    finshed display posts

--- bit_blog/src/view/components/page/SinglePost.js
+++ bit_blog/src/view/components/page/SinglePost.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { postsServices } from '../../../service/PostsService'
 import { authorsServices } from "../../../service/Service-authors"
-import { authorsEndpoint } from '../../../shared/constants'
 import RelatedPostList from './RelatedPostsList'
 
 class SinglePosts extends React.Component {
@@ -32,21 +31,37 @@ class SinglePosts extends React.Component {
 
     }
 
+    componentDidUpdate(prevProps) {
+        if (prevProps.match.params.id !== this.props.match.params.id) {
+            this.loadPost();
+        }
+    }
+
+    loadPost = () => {
+        postsServices.fetchSinglePost(this.props.match.params.id)
+            .then(postData => {
+                this.setState({
+                    singlePost: postData
+                })
+            })
+    }
+
 
     render() {
         if (!this.state.singlePost) {
             return <h1>Loading...</h1>
         }
+        const { authorId } = this.state.author
         return (
             <div>
                 <button className='back'><Link to='/posts/'>Back</Link></button>
                 <h3>{this.state.singlePost.title}</h3>
-                <h5><a>{this.state.author.name}</a></h5>
+                <h5><Link to={`/authors/${authorId}`}> {this.state.author.name}</Link></h5>
                 <p>{this.state.singlePost.body}</p>
                 <hr />
                 <RelatedPostList post={this.state.singlePost} />
                 <a></a>
-            </div>
+            </div >
         )
     }
 }
